578d5e1f0833793fafb037792e3b363a54ee9e55
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-11-20 18:23:14 -0800

    Ex 1 - my solution

---
+++ src/app/exercises/01-screensaver/[color]/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ScreenSaver from '../../../../components/ScreenSaver';
+
+function ScreenSaverExercise({params}) {
+	console.log(params);
+  return (
+    <main className="screen-saver-wrapper">
+      <ScreenSaver color={params.color} />
+    </main>
+  );
+}
+
+export default ScreenSaverExercise;
--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import Link from 'next/link';
 
 function ScreenSaverExercise() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your color:</p>
+      <ul>
+      	<li>
+      		<Link href="01-screensaver/hotpink">Hot Pink</Link>
+      	</li>
+      	<li>
+      		<Link href="01-screensaver/yellow">Yellow</Link>
+      	</li>
+      	<li>
+      		<Link href="01-screensaver/white">White</Link>
+      	</li>
+      </ul>
     </main>
   );
 }
