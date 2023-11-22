65ecbcf3fa1f018a2a70cc9b431d41c0a9e273b6
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-11-21 17:36:43 -0800

    Ex 2: my solution

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,21 @@
 'use client';
 import React from 'react';
+import { ToastContext } from '../../../../components/ToastProvider';
+import { useRouter } from 'next/navigation';
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    router.push('/exercises/02-flash-messages');
+    createToast('Your message was received!', 'success');
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import ToastProvider from '../../../components/ToastProvider';
+import ToastShelf from '../../../components/ToastShelf';
 
 import './styles.css';
 
 function FlashMsgLayout({ children }) {
   return (
+  	<ToastProvider>
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      	{children}
+      	<ToastShelf />
+      </body>
     </html>
+     </ToastProvider>
   );
 }
 
